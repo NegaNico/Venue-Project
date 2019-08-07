3bff51a4d166582064ef65c52640df37b983c6df
nlittle <nwl1991@outlook.com>
2019-08-06 19:37:42 -0500

    hey

---
+++ src/main/java/Capstone/MusicVenue/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Capstone.MusicVenue.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
